baa748efe95da664323023f3a64cc94d2ca9642d
Tirth02 <pateltirth0902@gmail.com>
2026-01-06 22:32:40 -0500

    advance scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,9 +10,42 @@ if(true)
 
 // console.log(a);
 // console.log(b);
-console.log(c); // that's why don't use var anywhere it shows no error 
+// console.log(c); // that's why don't use var anywhere it shows no error 
 
-console.log(a);
+// console.log(a);
+
+function one()
+{
+    const username = "tirth"
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log((website));
+
+    two();   
+}
+one();
+
+// ----------------interesting -----------------
+
+console.log(addOne(5));
+
+function addOne(num)
+{
+    return num + 1;
+}
+// console.log(addOne(5));
+
+console.log(addTwo(5)); // will show error
+const addTwo = function(num){
+    return num + 2;
+}
+
+// console.log(addTwo(5));
 
 
 
