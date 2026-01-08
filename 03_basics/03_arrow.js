252cb4f132b5da0b3216d5425f286d0b2c5b524b
Tirth02 <pateltirth0902@gmail.com>
2026-01-08 17:46:18 -0500

    this and arrow function in java

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Tirth",
+    price: 999,
+
+    welcomemessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomemessage()
+// user.username = "sam";
+// user.welcomemessage();
+
+function chai()
+{
+    // console.log(this);
+    let username = "Tirth"
+    console.log(this.username);
+    
+}
+
+// chai();
+
+
+//// -------------Arrow Function -------------
+
+const arrowChai = () =>{
+    console.log(this);
+}
+arrowChai()
+
+// explicit return
+const addTwo = (num1,num2) => {
+    return num1 + num2;
+}
+
+// implicit return 
+// const addTwo = (num1,num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => (num1 + num2) // no need when uses () instead of {}
+
+// const addTwo = (num1,num2) => ({username: "Tirth"}) // this is how you return object
+console.log(addTwo(4,5));
+
+console.log(add)
